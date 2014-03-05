9f4fbe15189af69128bcb8318ec2929a3fff8e21
reinhardfechter <reinhard.fechter@gmail.com>
2014-03-05 21:25:31 +0200

    Temp-visc-torque relationship successful fit

--- temp_der.py
+++ temp_der.py
@@ -7,8 +7,18 @@ def der_T(m,Cp,UA,T_inf,T):
 	der_T = (UA/(m*Cp))*(T_inf - T)
 	return der_T
 	
-def tau(k1,mu):
-	tau = k1*mu
-	return tau
+def der_Tm(k2,T,Tm):
+	der_Tm = k2*(T - Tm)
+	return der_Tm
 
+def mu(mu0,E,R,T):
+	from math import exp
+	mu = mu0*exp(E/(R*T))
+	return mu
+	
+def torque(k1,mu):
+	torque = k1*mu
+	return torque
+
+	
 
