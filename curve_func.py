148f74a8345b26837540250b40e4b656e8ba735e
reinhardfechter <reinhard.fechter@gmail.com>
2014-04-13 17:37:40 +0200

    wrapped code for optimization and opitimized a single run

--- curve_func.py
+++ curve_func.py
@@ -1,11 +1,9 @@
-# Function accepting parameters to give the temperature and torque curves
+# Functions accepting parameters to give the temperature and torque curves
 
-def temp_curve(p): # p -> parameters
+def temp_curve(time, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, UA, mu_0, E, q, prim_stab_0): # p -> parameters
 	from numpy import linspace, array, append, squeeze
 	from scipy.integrate import odeint
-	
-	k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, UA, mu_0, E, q = p
-	
+		
 	# remaining reaction kinetics parameters
 	n = 10.
 	
@@ -13,14 +11,11 @@ def temp_curve(p): # p -> parameters
 	HCl_0 = 0.
 	LDH_0 = 0.3
 	poly_act_0 = 5.
-	prim_stab_0 = 0.5
+	#prim_stab_0 = 0.5
 	
 	# initial temperatures
 	T_0 = 125.
 	Tm_0 = 200.
-	
-	#length of time
-	time_l = 20.87
 	
 	# stoichiometric matrix
 	stoyk  = array([[-n, -1., 0., 0., 0., 0., 0.],[1., 0., -1., 1., 0., 0., 0.],[1., 0., -1., 1., 0., 0., 0.],[0., 0., 0., -1., -1., 0., 0.],[0., 0., 0., -1., 0., 1., 0.],[0., 0., 0., -2., 0., 0., 1.]])
@@ -34,8 +29,6 @@ def temp_curve(p): # p -> parameters
 		HCl, LDH, poly_act, radical, prim_stab, deg_poly, x_link, T, Tm = A
 		r = array([r1(k3, HCl, LDH), r2(k4, HCl, poly_act), r3(k5, poly_act), r4(k6, radical, prim_stab), r5(k7, radical), r6(k8, radical)])
 		return append(squeeze(stoyk.T.dot(r)), [dTdt(T, mu_0, E, UA, k9, deg_poly, k10, x_link, q, k11), dTmdt(T, Tm, k2)])
-		
-	time = linspace(0, time_l, 100)
 	
 	A_0 = array([HCl_0, LDH_0, poly_act_0, 0., prim_stab_0, 0., 0., T_0, Tm_0])
 	
@@ -44,11 +37,11 @@ def temp_curve(p): # p -> parameters
 	return soln[:, 8]
 # __________________________________________________________________________________________________
 	
-def torque_curve(p): # p -> parameters
+def torque_curve(time, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, UA, mu_0, E, q, prim_stab_0):
 	from numpy import linspace, array, append, squeeze, zeros
 	from scipy.integrate import odeint
 	
-	k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, UA, mu_0, E, q = p
+	#k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, UA, mu_0, E, q = p
 	
 	# temperature curve parameters
 	R = 8.314
@@ -60,14 +53,11 @@ def torque_curve(p): # p -> parameters
 	HCl_0 = 0.
 	LDH_0 = 0.3
 	poly_act_0 = 5.
-	prim_stab_0 = 0.5
+	#prim_stab_0 = 0.5
 	
 	# initial temperatures
 	T_0 = 125.
 	Tm_0 = 200.
-	
-	#length of time
-	time_l = 20.87
 	
 	# stoichiometric matrix
 	stoyk  = array([[-n, -1., 0., 0., 0., 0., 0.],[1., 0., -1., 1., 0., 0., 0.],[1., 0., -1., 1., 0., 0., 0.],[0., 0., 0., -1., -1., 0., 0.],[0., 0., 0., -1., 0., 1., 0.],[0., 0., 0., -2., 0., 0., 1.]])
@@ -81,9 +71,7 @@ def torque_curve(p): # p -> parameters
 		HCl, LDH, poly_act, radical, prim_stab, deg_poly, x_link, T, Tm = A
 		r = array([r1(k3, HCl, LDH), r2(k4, HCl, poly_act), r3(k5, poly_act), r4(k6, radical, prim_stab), r5(k7, radical), r6(k8, radical)])
 		return append(squeeze(stoyk.T.dot(r)), [dTdt(T, mu_0, E, UA, k9, deg_poly, k10, x_link, q, k11), dTmdt(T, Tm, k2)])
-		
-	time = linspace(0, time_l, 100)
-	
+
 	A_0 = array([HCl_0, LDH_0, poly_act_0, 0., prim_stab_0, 0., 0., T_0, Tm_0])
 	
 	soln = odeint(dXdt, A_0, time)
