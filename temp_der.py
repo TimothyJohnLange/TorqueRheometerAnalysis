3835e06eedd7ba8992dea4ed08f9b2f114672854
reinhardfechter <reinhard.fechter@gmail.com>
2014-04-18 18:20:19 +0200

    limiting parameter with logistic function working and tabs removed from .py files

--- temp_der.py
+++ temp_der.py
@@ -2,12 +2,12 @@
 # of the system
 
 def dTdt(T, mu0, E, UA, k9, deg_poly, k10, x_link, q, k11):
-	from visc_torque_eq import mu
-	R = 8.314
-	m = 0.5
-	Cp = 900.
-	T_inf = 208.
-	return (UA/(m*Cp))*(T_inf - T) + k11*mu(mu0, E, R, T, k9, deg_poly, k10, x_link, q)
+    from visc_torque_eq import mu
+    R = 8.314
+    m = 0.5
+    Cp = 900.
+    T_inf = 208.
+    return (UA/(m*Cp))*(T_inf - T) + k11*mu(mu0, E, R, T, k9, deg_poly, k10, x_link, q)
 
 def dTmdt(T, Tm, k2):
     return k2*(T - Tm)
--- visc_torque_eq.py
+++ visc_torque_eq.py
@@ -1,8 +1,8 @@
 # Viscosity and torque equations
 
 def mu(mu0, E, R, T, k9, deg_poly, k10, x_link, q):
-	from math import exp
-	return mu0*exp(E/(R*T)) + k9*deg_poly + (k10*x_link)**q
+    from math import exp
+    return mu0*exp(E/(R*T)) + k9*deg_poly + (k10*x_link)**q
 
 def torque(k1, mu):
     return k1*mu
