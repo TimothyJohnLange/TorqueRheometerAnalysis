0ea7796f74218c20f7580a6b91ec96f995c42c7f
reinhardfechter <reinhard.fechter@gmail.com>
2014-05-23 12:42:30 +0200

    last commit before fixing multistart

--- model_parameters.py
+++ model_parameters.py
@@ -1,25 +1,28 @@
 # This set of parameters works the best so far
-def parameters(LDH_0):
+def parameters(ini_values):
     from lmfit import Parameters
+	
+    k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, UA, mu_0, E, q, prim_stab_0, LDH_0 = ini_values
+	
     p = Parameters()
-    #          (         Name,  Value,  Vary,    Min,     Max)    
-    p.add_many((         'k1',      2,  True,    0.0,     2.8),
-               (         'k2',    5.0,  True,    0.0, 20000.0),
-               (         'k3',   0.35,  True,    0.0,    6.83),
-               (         'k4',    0.3,  True,    0.0,     2.0),
-               (         'k5',  0.001,  True,    0.0,    0.05),
-               (         'k6',    1.0,  True,    0.0,    30.0),
-               (         'k7',    0.1,  True,    0.0,     4.0),
-               (         'k8',   0.05,  True,    0.0,    10.0),
-               (         'k9',    2.0,  True,    0.0,    14.0),
-               (        'k10',    2.0,  True,    0.5,    8.79),
-               (        'k11',    1.0,  True,    0.0,     8.0),
-               (         'UA',  340.0,  True,  100.0,   400.0),
-               (       'mu_0',   0.03,  True,    0.0,     0.1),
-               (          'E', 6500.0,  True, 5000.0,    None),
-               (          'q',    7.0, False,    0.0,    17.0),
-               ('prim_stab_0',    0.7,  True,    0.5,     1.0),
-               (      'LDH_0',  LDH_0, False,   None,    None))
+    #          (         Name,       Value,  Vary,    Min,     Max)    
+    p.add_many((         'k1',          k1,  True,    0.0,     2.8),
+               (         'k2',          k2,  True,    0.0, 20000.0),
+               (         'k3',          k3,  True,    0.0,    6.83),
+               (         'k4',          k4,  True,    0.0,     2.0),
+               (         'k5',          k5,  True,    0.0,    0.05),
+               (         'k6',          k6,  True,    0.0,    30.0),
+               (         'k7',          k7,  True,    0.0,     4.0),
+               (         'k8',          k8,  True,    0.0,    10.0),
+               (         'k9',          k9,  True,    0.0,    14.0),
+               (        'k10',         k10,  True,    0.5,    8.79),
+               (        'k11',         k11,  True,    0.0,     8.0),
+               (         'UA',          UA,  True,  100.0,   400.0),
+               (       'mu_0',        mu_0, False,    0.0,     0.1),
+               (          'E',           E, False, 5000.0,    None),
+               (          'q',           q, False,    0.0,    17.0),
+               ('prim_stab_0', prim_stab_0, False,    0.5,     1.3),
+               (      'LDH_0',       LDH_0, False,   None,    None))
     return p
 	
 def unpack_parameters(p):
@@ -86,3 +89,7 @@ def parameter_vectors(all_ps):
         LDH_0_all = append(LDH_0_all, LDH_0)
 
     return k1_all, k2_all, k3_all, k4_all, k5_all, k6_all, k7_all, k8_all, k9_all, k10_all, k11_all, UA_all, mu_0_all, E_all, q_all, prim_stab_0_all, LDH_0_all
+
+def ms_var_func(val, factor):
+    from random import random
+    return val + random()*val*factor - val*factor/2.0
