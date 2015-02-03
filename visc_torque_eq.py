dc134ecdbcb3fd33e544b15d66b3a5ca430f3f95
reinhardfechter <reinhard.fechter@gmail.com>
2015-02-03 10:38:53 +0200

    testing modification to model

--- visc_torque_eq.py
+++ visc_torque_eq.py
@@ -2,7 +2,7 @@
 
 def mu(mu0, E, R, T, k9, deg_poly, k10, x_link, q):
     from math import exp
-    return mu0*exp(E/(R*T)) + k9*deg_poly + (k10*x_link)**q
+    return mu0*exp(E/(R*T)) + k9*(deg_poly + 2*x_link) + (k10*x_link)**q
 
 def torque(k1, mu):
     return k1*mu
